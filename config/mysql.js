9a1c53c9d87fec0bf32d5c7d8f9f980e63e0a96d
nurmuhamadsoleh <nursoleh33@gmail.com>
2022-10-29 11:28:17 +0700

    update

--- config/mysql.js
+++ config/mysql.js
@@ -1,9 +1,13 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host: "localhost", //akses untuk database
-  user: "root",
-  password: "Sukses889123",
-  database: "karyawan_crud",
+  // host: "localhost", //akses untuk database
+  // user: "root",
+  // password: "Sukses889123",
+  // database: "karyawan_crud",
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
 });
 
 module.exports = connection;
