5d755b50e3760e512f78eb0b71c68aeefddb57dd
nurmuhamadsoleh <nursoleh33@gmail.com>
2022-10-29 11:19:49 +0700

    update dialect

--- config/sequelize.js
+++ config/sequelize.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  dialect: process.env.DB_DIALECT,
+  // dialect: process.env.DB_DIALECT,
 });
 
 //untuk mengecek connection ke database
