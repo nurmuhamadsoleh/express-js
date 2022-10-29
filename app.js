b38425508e8eb2481a11113df865deb399d437d4
nurmuhamadsoleh <nursoleh33@gmail.com>
2022-10-29 11:23:03 +0700

    update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true })); //karena post sama get sama dal
 
 // running express
 // app.listen(3000, () => console.log(`Server: http://localhost:3000`));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.$PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
